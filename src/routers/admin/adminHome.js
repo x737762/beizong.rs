9bc841a96aa6ff41549bdd6ee23944f65e319c6b
Best Shi <bestshi@qq.com>
2023-07-31 23:26:00 +0800

    后台基本样式

--- src/routers/admin/adminHome.js
+++ src/routers/admin/adminHome.js
@@ -1,5 +1,16 @@
 const adminHome = async (ctx) => {
-  await ctx.render("adminHome");
+  console.log(111, ctx.query);
+  const data = {
+    menuId: ctx.query?.menuId ?? "1",
+    list: [
+      { id: "1", name: "首页" },
+      { id: "2", name: "文章管理" },
+      { id: "3", name: "随笔管理" },
+      { id: "4", name: "分类管理" },
+      { id: "5", name: "菜单管理" },
+    ],
+  };
+  await ctx.render("adminHome", data);
 };
 
 export default adminHome;
